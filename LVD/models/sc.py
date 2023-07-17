6cf286ac15adc69284ee02056b44a2fd07276d8d
magenta1223 <magenta1223@naver.com>
2023-07-17 16:45:58 +0900

    bugfix

--- LVD/models/sc.py
+++ LVD/models/sc.py
@@ -89,15 +89,18 @@ class StateConditioned_Model(BaseModel):
         fixed_dist = get_fixed_dist(q_clone, tanh = self.tanh)
 
         if self.tanh:
-            z_normal, z = post.rsample_with_pre_tanh_value()
-            self.outputs['z'] = z.clone().detach()
-            self.outputs['z_normal'] = z_normal.clone().detach()
+            skill_normal, skill = post.rsample_with_pre_tanh_value()
+            self.outputs['z'] = skill.clone().detach()
+            self.outputs['z_normal'] = skill_normal.clone().detach()
         else:
-            z = post.rsample()
-            self.outputs['z'] = z.clone().detach()
+            skill = post.rsample()
+            self.outputs['z'] = skill.clone().detach()
         
         # Skill Decoder 
-        decode_inputs = self.dec_input(states.clone(), z, self.Hsteps)
+        if self.manipulation:
+            decode_inputs = self.dec_input(states[:, :, :self.n_pos].clone(), skill, self.Hsteps)
+        else:
+            decode_inputs = self.dec_input(states.clone(), skill, self.Hsteps)
 
         N, T = decode_inputs.shape[:2]
         skill_hat = self.skill_decoder(decode_inputs.view(N * T, -1)).view(N, T, -1)
